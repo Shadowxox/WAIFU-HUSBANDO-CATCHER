fac69169f525f905599e6e61a48d621b62935013
Shadowxox <shadowx0x12@gmail.com>
2025-05-08 00:03:04 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,19 +3,21 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7795212861"
-    sudo_users = "7361967332", "5758240622", "7795212861"
-    GROUP_ID = -1002643948280
+    sudo_users = "7795212861", "5758240622", "7361967332"
+    GROUP_ID = -1002680733244
+
     TOKEN = "7525650740:AAHujfrXw8A4F3YthCemnWm5re28AbPcYBY"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://i.ibb.co/RGm4r4q0/image.jpg", "https://i.ibb.co/Kcxbdcm4/image.jpg"]
-    SUPPORT_CHAT = "hwkwjieie"
-    UPDATE_CHAT = "hwkwjieie"
-    BOT_USERNAME = "Shadow_testingbot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
+    PARTNER = "7361967332", "5758240622", "7795212861"
+    PHOTO_URL = ["https://files.catbox.moe/h0qot3.jpg", "https://files.catbox.moe/tx0o73.jpg", "https://files.catbox.moe/rqwt42.jpg"]
+    SUPPORT_CHAT = "-1002680733244"
+    UPDATE_CHAT = "-1002680733244"
+    BOT_USERNAME = "@Shadow_testingbot"
+    CHARA_CHANNEL_ID = "-1002474756169"
     api_id = 23287799
     api_hash = "9f4f17dae2181ee22c275b9b40a3c907"
 
-    
+
 class Production(Config):
     LOGGER = True
 

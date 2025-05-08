60b986740779b1fab5ec164c2dfd91bdec36f56a
Shadowxox <shadowx0x12@gmail.com>
2025-05-08 19:59:10 +0530

    Update broadcast.py

--- shivu/modules/broadcast.py
+++ shivu/modules/broadcast.py
@@ -1,37 +1,36 @@
 from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler 
+from telegram.ext import CallbackContext, CommandHandler
 
-from shivu import application, OWNER_ID
-from shivu import  top_global_groups_collection , pm_users
+from shivu import application, top_global_groups_collection, pm_users, is_sudo
 
 async def broadcast(update: Update, context: CallbackContext) -> None:
-
-    if update.effective_user.id != 7481211733:
-        await update.message.reply_text("You are not authorized to use this command.")
+    user_id = update.effective_user.id
+    if not is_sudo(user_id):
+        await update.message.reply_text("❌ You are not authorized to use this command.")
         return
 
     message_to_broadcast = update.message.reply_to_message
-
     if message_to_broadcast is None:
-        await update.message.reply_text("Please reply to a message to broadcast.")
+        await update.message.reply_text("❗ Please reply to a message to broadcast.")
         return
 
     all_chats = await top_global_groups_collection.distinct("group_id")
     all_users = await pm_users.distinct("_id")
 
-    shuyaa = list(set(all_chats + all_users))
-
+    targets = list(set(all_chats + all_users))
     failed_sends = 0
 
-    for chat_id in shuyaa:
+    for chat_id in targets:
         try:
-            await context.bot.forward_message(chat_id=chat_id,
-                                              from_chat_id=message_to_broadcast.chat_id,
-                                              message_id=message_to_broadcast.message_id)
+            await context.bot.forward_message(
+                chat_id=chat_id,
+                from_chat_id=message_to_broadcast.chat_id,
+                message_id=message_to_broadcast.message_id
+            )
         except Exception as e:
-            print(f"Failed to send message to {chat_id}: {e}")
+            print(f"Failed to send to {chat_id}: {e}")
             failed_sends += 1
 
-    await update.message.reply_text(f"Broadcast complete. Failed to send to {failed_sends} chats/users.")
+    await update.message.reply_text(f"✅ Broadcast complete.\n❌ Failed to send to {failed_sends} users/chats.")
 
 application.add_handler(CommandHandler("broadcast", broadcast, block=False))
